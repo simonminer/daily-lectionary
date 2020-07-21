dbb7b7431cd8b67dd51dca16c40a7dede3f1cac4
Simon Miner <simon.miner@gmail.com>
2020-07-21 09:23:34 -0400

    Enhanced logic to compute next data in script to fetch readings

--- scripts/get_daily_lectionary_references.pl
+++ scripts/get_daily_lectionary_references.pl
@@ -4,6 +4,7 @@ use warnings;
 use Getopt::Long;
 use LWP::Simple qw( get );
 use JSON;
+use Time::Local;
 
 my ( $day, $month, $year ) = ( localtime() )[ 3 .. 5 ];
 my $today = sprintf( "%04d-%02d-%02d", $year + 1900, $month + 1, $day );
@@ -29,8 +30,13 @@ while ( 1 ) {
 
     # Fetch the current dates lectionary data.
     my $url  = "$args{'base-url'}/$current_date";
-    my $data = JSON::from_json( get( $url ) );
-    die "No data returned from request $url.\n" unless $data;
+    my $json = get( $url );
+    if ( !$json ) {
+        warn "No data returned from request $url.\n" unless $json;
+        $current_date = next_date( $current_date );
+        next;
+    }
+    my $data = JSON::from_json( $json );
 
     # Parse out and print the Scripture references for each reading.
     my $readings = $data->{daily}->{readings};
@@ -42,5 +48,25 @@ while ( 1 ) {
 
     # Increment the current date if we haven't reach the end.
     last if $current_date eq $args{end};
-    $current_date = $data->{daily}->{tomorrow};
+    $current_date = next_date( $current_date, $data );
+}
+
+sub next_date {
+    my( $current_date, $data ) = @_;
+    
+    # Use the next date given in the web service payload...
+    my $next_date;
+    if ( $data ) {
+        $next_date = $data->{daily}->{tomorrow};
+    }
+
+    # ...or compute the next date ourselves.
+    else {
+        my( $year, $month, $day ) = ( $current_date =~ m/^(\d{4})\-(\d{2})\-(\d{2})$/ );
+        my $time = timelocal( 0, 0, 0, $day, $month - 1, $year ) + 60 * 60 * 24;
+        ( $day, $month, $year ) = ( localtime( $time ) )[3..5];
+        $next_date = sprintf( "%04d-%02d-%02d", $year + 1900, $month + 1, $day );
+    }
+
+    return $next_date;
 }
